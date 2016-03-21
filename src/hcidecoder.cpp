1beadf3863374bdba3634dfc8984f40a4f3f7de4
Bertrand Martel <bmartel.fr@gmail.com>
2016-03-21 02:47:19 +0100

    doxygen documentation

--- src/hcidecoder.cpp
+++ src/hcidecoder.cpp
@@ -1,26 +1,26 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Bertrand Martel
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+/************************************************************************************
+ * The MIT License (MIT)                                                            *
+ *                                                                                  *
+ * Copyright (c) 2015 Bertrand Martel                                               *
+ *                                                                                  * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy     * 
+ * of this software and associated documentation files (the "Software"), to deal    * 
+ * in the Software without restriction, including without limitation the rights     * 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        * 
+ * copies of the Software, and to permit persons to whom the Software is            * 
+ * furnished to do so, subject to the following conditions:                         * 
+ *                                                                                  * 
+ * The above copyright notice and this permission notice shall be included in       * 
+ * all copies or substantial portions of the Software.                              * 
+ *                                                                                  * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       * 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         * 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      * 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           * 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    * 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        * 
+ * THE SOFTWARE.                                                                    * 
+ ************************************************************************************/
 /**
 	hcidecoder.cpp
 	HCI decoder
@@ -37,8 +37,7 @@
 
 using namespace std;
 
-HciDecoder::HciDecoder()
-{
+HciDecoder::HciDecoder(){
 }
 
 HciDecoder::~HciDecoder()
@@ -50,10 +49,24 @@ HciDecoder::~HciDecoder()
 	frame_list.clear();
 }
 
+/**
+ * @brief
+ *      get all decoded packet
+ * @return
+ *      all decoded packet
+ */
 std::vector<IHciFrame*> HciDecoder::getFrameList(){
 	return frame_list;
 }
 
+/**
+ * @brief
+ *      convert all decoded frame to json
+ * @param beautify
+ *      add indentation + linefeed
+ * @return
+ *      all decoded packet in json format
+ */
 std::string HciDecoder::toJson(bool beautify){
 
 	Json::Value output(Json::arrayValue);
@@ -71,6 +84,14 @@ std::string HciDecoder::toJson(bool beautify){
 	}
 }
 
+/**
+ * @brief
+ *      decode one HCI frame
+ * @param data
+ *      data to be parsed (length can be > packet size)
+ * @return
+ *      Hci frame object decoded or null pointer if not parsed correctly
+ */
 IHciFrame* HciDecoder::decode(std::vector<char> data){
 
 	IHciFrame * frame = 0;
